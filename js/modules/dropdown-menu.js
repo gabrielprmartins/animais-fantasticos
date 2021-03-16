eb3b4d7d5de2102ca8deeda69fe5f5be2d9acfa6
Gabriel Pereira <gabrielpr.martins@gmail.com>
2021-03-16 18:15:44 -0300

    Primeiro Commit

---
+++ js/modules/dropdown-menu.js
@@ -0,0 +1,19 @@
+import initOutsideClick from './outsideclick.js';
+
+export default function initDropDownMenu() {
+  const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+
+  function handleClick(event) {
+    event.preventDefault();
+    this.classList.add('active');
+    initOutsideClick(this, ['touchstart', 'click'], () => {
+      this.classList.remove('active');
+    });
+  }
+
+  dropdownMenus.forEach((menu) => {
+    ['touchstart', 'click'].forEach((userEvent) => {
+      menu.addEventListener(userEvent, handleClick);
+    });
+  });
+}
